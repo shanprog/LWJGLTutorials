d0dd6a07b46827d2bbca8b6769457d2f80f8992b
ShAN <shan.prog@gmail.com>
2015-12-10 11:51:50 +0900

    Добавлен 12  урок от Oskar Veerhoek - Игра Minecraft 2D - выбор блока

--- src/codes/oskarveerhoek/episode11/Boot.java
+++ src/codes/oskarveerhoek/episode11/Boot.java
@@ -1,6 +1,7 @@
 package codes.oskarveerhoek.episode11;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
@@ -11,7 +12,9 @@ import java.io.File;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Boot {
-    BlockGrid grid;
+
+    private BlockGrid grid;
+    private BlockType selection = BlockType.STONE;
 
     public Boot() {
         try {
@@ -59,7 +62,7 @@ public class Boot {
         if (mouseClicked) {
             int grid_x = Math.round(mouseX / World.BLOCK_SIZE);
             int grid_y = Math.round(mouseY / World.BLOCK_SIZE);
-            grid.setAt(grid_x, grid_y, BlockType.STONE);
+            grid.setAt(grid_x, grid_y, selection);
         }
 
 
@@ -70,6 +73,31 @@ public class Boot {
 
             if (Keyboard.getEventKey() == Keyboard.KEY_L) {
                 grid.load(new File("save.xml"));
+            }
+
+            if (Keyboard.getEventKey() == Keyboard.KEY_1) {
+                selection = BlockType.STONE;
+            }
+
+            if (Keyboard.getEventKey() == Keyboard.KEY_2) {
+                selection = BlockType.DIRT;
+            }
+
+            if (Keyboard.getEventKey() == Keyboard.KEY_3) {
+                selection = BlockType.GRASS;
+            }
+
+            if (Keyboard.getEventKey() == Keyboard.KEY_4) {
+                selection = BlockType.AIR;
+            }
+
+            if (Keyboard.getEventKey() == Keyboard.KEY_C) {
+                grid.clear();
+            }
+
+            if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+                Display.destroy();
+                System.exit(0);
             }
         }
     }
